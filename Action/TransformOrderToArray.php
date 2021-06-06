4f8b7d4d97ae4190ce21e1e9b8aa5366a32828ca
tajveez <tajveez786@gmail.com>
2021-06-06 20:29:54 +0500

    making dataCollectors

---
+++ Action/TransformOrderToArray.php
@@ -0,0 +1,31 @@
+<?php
+declare (strict_types = 1);
+
+namespace Bluebird\OrderExport\Action;
+use Bluebird\OrderExport\Model\HeaderData;
+use Magento\Sales\Api\OrderRepositoryInterface;
+
+class TransformOrderToArray {
+	private $orderRepository;
+	private $dataCollectors;
+	public function __construct(OrderRepositoryInterface $orderRepository,
+		array $dataCollectors) {
+		$this->orderRepository = $orderRepository;
+		$this->dataCollectors = $dataCollectors;
+	}
+
+	public function execute(int $orderId,
+		HeaderData $headerData) {
+		$order = $this->orderRepository->get($orderId);
+		$output = [];
+
+		foreach ($this->dataCollectors as $collector) {
+			if (!($collector instanceof DataCollectorInterface)) {
+				continue;
+			}
+
+			$output = array_merge($output, $collector->collect($order, $headerData));
+		}
+		return $output;
+	}
+}
