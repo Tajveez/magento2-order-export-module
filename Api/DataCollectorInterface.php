87725a18a243a31950b0cc68d6d6ac8efd86fc50
tajveez <tajveez786@gmail.com>
2021-06-06 21:22:27 +0500

    Api Interface Added

---
+++ Api/DataCollectorInterface.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Bluebird\OrderExport\Api;
+use Bluebird\OrderExport\Model\HeaderData;
+use Magento\Sales\Api\Data\OrderInterface;
+
+interface DataCollectorInterface {
+    public function collect(OrderInterface $order, HeaderData $headerData): array
+}
--- Controller/Adminhtml/Export/Run.php
+++ Controller/Adminhtml/Export/Run.php
@@ -1,30 +1,31 @@
 <?php
 
 namespace Bluebird\OrderExport\Controller\Adminhtml\Export;
+use Bluebird\OrderExport\Model\HeaderDataFactory;
 use Magento\Backend\App\Action;
 use Magento\Backend\App\Action\Context;
-
 use Magento\Framework\App\Action\HttpPostActionInterface;
 use Magento\Framework\Controller\Result\JsonFactory;
-use Bluebird\OrderExport\Model\HeaderDataFactory;
 
 class Run extends Action implements HttpPostActionInterface {
 
-    private $jsonFactory;
-    private $headerDataFactory;
+	private $jsonFactory;
+	private $headerDataFactory;
 
-    public function __construct(JsonFactory $jsonFactory, Context $context, HeaderDataFactory $headerDataFactory) {
-        parent::__construct($context);
-        $this->jsonFactory = $jsonFactory;
-        $this->headerDataFactory = $headerDataFactory;
-    }
+	public function __construct(JsonFactory $jsonFactory, Context $context, HeaderDataFactory $headerDataFactory) {
+		parent::__construct($context);
+		$this->jsonFactory = $jsonFactory;
+		$this->headerDataFactory = $headerDataFactory;
+	}
 
-    public function execute()
-    {
-        $headerData = $this->headerDataFactory->create();
-        $headerData->setShipDate(new \DateTime($this->getRequest()->getParam('ship_date')));
-        $headerData->setMerchantNotes(\htmlspecialchars($this->getRequest()->getParam('merchant_notes')));
+	public function execute() {
+		$headerData = $this->headerDataFactory->create();
+		$headerData->setShipDate(new \DateTime($this->getRequest()->getParam('ship_date')));
+		$headerData->setMerchantNotes(\htmlspecialchars($this->getRequest()->getParam('merchant_notes')));
 
-        return $this->jsonFactory->create([]);
-    }
+		return $this->jsonFactory->create([
+			'success' => true,
+			'message' => 'File Exported Successful',
+		]);
+	}
 }
