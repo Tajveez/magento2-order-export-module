87ae3929be25df2901283909508e9d32f82874ad
tajveez <tajveez786@gmail.com>
2021-06-06 14:14:08 +0500

    HeaderData model class usage

--- Controller/Adminhtml/Export/Run.php
+++ Controller/Adminhtml/Export/Run.php
@@ -6,16 +6,25 @@ use Magento\Backend\App\Action\Context;
 
 use Magento\Framework\App\Action\HttpPostActionInterface;
 use Magento\Framework\Controller\Result\JsonFactory;
+use Bluebird\OrderExport\Model\HeaderDataFactory;
 
 class Run extends Action implements HttpPostActionInterface {
 
-    public function __construct(JsonFactory $jsonFactory, Context $context) {
+    private $jsonFactory;
+    private $headerDataFactory;
+
+    public function __construct(JsonFactory $jsonFactory, Context $context, HeaderDataFactory $headerDataFactory) {
         parent::__construct($context)
         $this->jsonFactory = $jsonFactory;
+        $this->headerDataFactory = $headerDataFactory;
     }
 
     public function execute()
     {
+        $headerData = $this->headerDataFactory->create();
+        $headerData->setShipDate(new \DateTime($this->getRequest()->getParam('ship_date')));
+        $headerData->setMerchantNotes(\htmlspecialchars($this->getRequest()->getParam('merchant_notes')));
+
         return $this->jsonFactory->create([]);
     }
 }
